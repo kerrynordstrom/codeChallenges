f242aac91555859566881daa8087171a40720e3c
Kerry Nordstrom <kerry.a.nordstrom@gmail.com>
2018-03-17 22:37:36 -0500

    Adding solution

---
+++ kebabize.js
@@ -0,0 +1,5 @@
+function kebabize(str) {
+  str = str.replace(/[\d]/g, '')
+  str = str.charAt(0).toLowerCase() + str.slice(1);
+  return str.replace(/([A-Z])/g, char => `-${char.toLowerCase()}`)
+}

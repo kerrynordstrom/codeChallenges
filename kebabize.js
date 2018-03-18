e50d3108f4f4ddb524e319f73e3ec16363db4607
Kerry Nordstrom <kerry.a.nordstrom@gmail.com>
2018-03-17 22:38:34 -0500

    Adding solution

--- kebabize.js
+++ kebabize.js
@@ -1,5 +1,5 @@
 function kebabize(str) {
   str = str.replace(/[\d]/g, '')
   str = str.charAt(0).toLowerCase() + str.slice(1);
-  return str.replace(/([A-Z])/g, char => `-${char.toLowerCase()}`)
+  return str.replace(/([A-Z])/g, char => `-${char.toLowerCase()}`); 
 }

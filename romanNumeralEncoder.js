8cdd4bfc1685c5652553f66b414eccc5e2ce8d93
Kerry Nordstrom <kerry.a.nordstrom@gmail.com>
2018-03-28 11:36:45 -0500

    Adding roman numeral encoder

---
+++ romanNumeralEncoder.js
@@ -0,0 +1,30 @@
+function solution(number) {
+
+  const encoder = {
+    M: 1000,
+    CM: 900,
+    D: 500,
+    CD: 400,
+    C: 100,
+    XC: 90,
+    L: 50,
+    XL: 40,
+    X: 10,
+    IX: 9,
+    V: 5,
+    IV: 4,
+    I: 1
+  }
+
+  let roman = '';
+
+  for (let i in encoder) {
+
+    while (number >= encoder[i]) {
+      roman += i;
+      number -= encoder[i];
+    }
+
+  }
+  return roman;
+}

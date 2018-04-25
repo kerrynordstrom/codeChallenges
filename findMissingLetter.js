dafad105374cfd16c2cb81e5729396a540b1eb2d
Kerry Nordstrom <kerry.a.nordstrom@gmail.com>
2018-04-25 17:45:30 -0500

    adding find missing letter solution

---
+++ findMissingLetter.js
@@ -0,0 +1,9 @@
+function findMissingLetter(array) {
+  let str = array.join('');
+
+  for (let i = 0; i < str.length - 1; i++) {
+    if (str.charCodeAt(i + 1) - str.charCodeAt(i) !== 1) {
+      return String.fromCharCode(str.charCodeAt(i) + 1);
+    }
+  }
+}
